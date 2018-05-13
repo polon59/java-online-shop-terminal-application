0433d8ed6c7663c1e71fd38228def9d2f0f1dc44
polon59 <przemyslaw.polczak@gmail.com>
2018-05-13 19:44:01 +0200

    add new class file

---
+++ frompythontojava/onlineshop/part1/ProductIterator.java
@@ -0,0 +1,14 @@
+package frompythontojava.onlineshop.part1;
+
+import java.util.Iterator;
+
+public class ProductIterator implements Iterator{
+    
+    public boolean hasNext(){
+
+    }
+
+    public Product next(){
+        
+    }
+}
