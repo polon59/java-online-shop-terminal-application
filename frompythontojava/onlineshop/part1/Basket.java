826276aa7c8c86f1f36cb700f7d34a044994eb34
polon59 <przemyslaw.polczak@gmail.com>
2018-05-13 19:43:11 +0200

    add new class file

---
+++ frompythontojava/onlineshop/part1/Basket.java
@@ -0,0 +1,30 @@
+package frompythontojava.onlineshop.part1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Basket{
+    private List<Product> productList;
+    private Iterator<Product> iterator;
+
+    public Basket(){
+        this.productList = new ArrayList<>();
+    }
+
+    public List<Product> getProducts(){
+        return this.productList;
+    }
+
+    public Iterator<Product> getIterator(){
+        return this.iterator;
+    }
+
+    public void addProduct(Product product){
+        this.productList.add(product);
+    }
+
+    public boolean removeProduct(Product product){
+        this.productList.remove(product);
+    }
+}
