cda02b6169b40f6d4682744a957ec3786db3f26c
polon59 <przemyslaw.polczak@gmail.com>
2018-05-13 19:43:35 +0200

    add new class file

---
+++ frompythontojava/onlineshop/part1/ProductCategory.java
@@ -0,0 +1,33 @@
+package frompythontojava.onlineshop.part1;
+
+import java.time.LocalDate;
+
+public class ProductCategory{
+    String name;
+    int ID;
+    ProductCategoryStatus status;
+
+    public ProductCategory(){
+
+    }
+
+    public ProductCategory(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setPromoted(LocalDate expirationDate){
+
+    }
+
+    public void setNonPromoted(){
+
+    }
+
+    public ProductCategoryStatus getStatus(){
+        return this.status;
+    }
+}
