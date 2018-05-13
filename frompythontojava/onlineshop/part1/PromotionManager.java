bdbf7b880c513de7f7e80e6bbc6a328611bf542d
polon59 <przemyslaw.polczak@gmail.com>
2018-05-13 19:44:37 +0200

    add new class file

---
+++ frompythontojava/onlineshop/part1/PromotionManager.java
@@ -0,0 +1,25 @@
+package frompythontojava.onlineshop.part1;
+
+public class PromotionManager{
+
+    Products products;
+    PromotedProducts promotedProducts;
+
+    public void promoteProductCategory(ProductCategory productCategory){
+
+    }
+
+    public void unpromoteProductCategory(ProductCategory productCategory){
+
+    }
+
+    public void promoteProductsAfterPromotion(){
+
+    }
+
+    private void moveProduct(Product product, Products from, Products to){
+
+    }
+
+
+}
