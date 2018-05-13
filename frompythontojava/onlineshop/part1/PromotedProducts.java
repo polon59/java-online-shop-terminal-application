761455023124aff1d40a867f8b704af6b41befd6
polon59 <przemyslaw.polczak@gmail.com>
2018-05-13 19:44:29 +0200

    add new class file

---
+++ frompythontojava/onlineshop/part1/PromotedProducts.java
@@ -0,0 +1,11 @@
+package frompythontojava.onlineshop.part1;
+import java.util.List;
+
+import frompythontojava.onlineshop.part1.Products;
+
+public class PromotedProducts extends Products{
+
+    public List<Product> getProductsAfterPromotion(){
+        
+    }
+}
