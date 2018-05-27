2e54ee172737436aa7eead8fdf0789d610c99516
polon59 <przemyslaw.polczak@gmail.com>
2018-05-27 22:25:44 +0200

    create Main class to launch application

---
+++ frompythontojava/onlineshop/part3/Main.java
@@ -0,0 +1,9 @@
+import frompythontojava.onlineshop.part3.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application onlineShop = new Application();
+        onlineShop.LaunchMainMenu();
+    }
+}
